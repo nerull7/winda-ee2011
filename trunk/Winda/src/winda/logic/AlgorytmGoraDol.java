145ff737233c70cf42688e32e42c7812dc02a151
PGrondek@gmail.com <PGrondek@gmail.com@c9243cdb-3605-be4a-6eca-9358810d06b3>
2011-03-23 09:30:27 +0000

    Poprawiono nazwy paczek na bardziej normalne

---
+++ trunk/Winda/src/winda/logic/AlgorytmGoraDol.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package winda.logic;
+
+/**
+ *
+ * @author Tomek
+ */
+public class AlgorytmGoraDol implements IAlgorytm {
+
+    public int[] Trasa(Pasazer[] pasazerowie) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ trunk/Winda/src/winda/logic/IAlgorytm.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package winda.logic;
+
+/**
+ *
+ * @author Tomek
+ */
+public interface IAlgorytm {
+    public int [] Trasa(Pasazer [] pasazerowie);
+}
