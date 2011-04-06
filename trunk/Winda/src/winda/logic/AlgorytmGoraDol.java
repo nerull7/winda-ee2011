17808fe0128cf3174dad8afcfab3bda132b8964a
PGrondek@gmail.com <PGrondek@gmail.com@c9243cdb-3605-be4a-6eca-9358810d06b3>
2011-04-06 19:58:15 +0000

    M    Winda/src/winda/logic/AlgorytmGoraDol.java A    Winda/src/winda/logic/PasazerSort.java

--- trunk/Winda/src/winda/logic/AlgorytmGoraDol.java
+++ trunk/Winda/src/winda/logic/AlgorytmGoraDol.java
@@ -5,18 +5,88 @@
 
 package winda.logic;
 
+import java.util.ArrayList;
+import sun.misc.Sort;
+
 /**
- *
- * @author Tomek
+ * Algortym poruszania się windą przy wsiadaniu pasażerów
+ * jadących tylko w kierunku w którym porusza się winda.
+ * @author Przemo
  */
 public class AlgorytmGoraDol implements IAlgorytm {
+    private int start_floor;
+    private int floor_count;
+    private ArrayList<Integer> floors;
+    private Pasazer[] pasazerowie;
+    private int acctual_floor;
+
+    public AlgorytmGoraDol(int floor_count){
+        this.start_floor = 0;
+        this.floor_count = floor_count;
+    }
+
+    public AlgorytmGoraDol(int floor_count, int start_floor){
+        this.floor_count = floor_count;
+        this.start_floor = start_floor;
+    }
+
+    public void setStartFloor(int start_floor){
+        this.start_floor = start_floor;
+    }
+
+    public int getStartFloor(){
+        return this.start_floor;
+    }
+
+    public void setFloorCount(int floor_count){
+        this.floor_count = floor_count;
+    }
+
+    public int getFloorCount(int floor_count){
+        return this.floor_count;
+    }
+
+    private void sortUp(ArrayList<Integer> array){
+        Sort.quicksort(array.toArray(), null);
+    }
+
+    private void goUp(){
+        int i =0;
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+        while(this.pasazerowie[i].GetStart()<this.acctual_floor)
+            i++;
+        while(i<this.pasazerowie.length)
+            if(this.pasazerowie[i].GetStart()<this.pasazerowie[i].GetStop()){
+                if(tmp.contains(this.pasazerowie[i].GetStart()))
+                    tmp.add(this.pasazerowie[i].GetStart());
+                if(tmp.contains(this.pasazerowie[i].GetStop()))
+                    tmp.add(this.pasazerowie[i].GetStop());
+            }
+        this.sortUp(tmp);
+        this.floors.addAll(tmp);
+    }
+
+    public int[] setCourse(){
+        this.floors = new ArrayList<Integer>();
+        floors.add(this.start_floor);
+
+        PasazerSort sort = new PasazerSort(pasazerowie);
+        sort.sortByStart();
+        this.acctual_floor = this.start_floor;
+
+        int []ret = new int[floors.size()];
+        for(int i=0;i<floors.size();i++)
+            ret[i] = floors.get(i).intValue();
+        return ret;
+    }
 
     public int[] Trasa(Pasazer[] pasazerowie) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.pasazerowie = pasazerowie;
+        return this.setCourse();
     }
 
     public void SetMaxPietro(int maxPietro) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.setFloorCount(floor_count);
     }
 
 }
